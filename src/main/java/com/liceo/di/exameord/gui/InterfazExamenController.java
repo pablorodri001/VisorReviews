3b1dd2b6105e22ee09ba3bee64da50bbc421f635
Pablo Rodriguez rodriguez <pablorodriguezrodriguez@MacBook-Air-de-Pablo.local>
2024-03-14 09:51:55 +0100

    Añade un comentario sin fecha

--- src/main/java/com/liceo/di/exameord/gui/InterfazExamenController.java
+++ src/main/java/com/liceo/di/exameord/gui/InterfazExamenController.java
@@ -6,13 +6,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.util.converter.LocalDateStringConverter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class InterfazExamenController {
 
-
+    @FXML
+    public TextField contenido;
+    @FXML
+    public TextField NombreAutor;
+    @FXML
+    public TextField fecha;
     // You should place this line of code in the Class
     // where you like to use the FacadeApp
     FacadeApp fApp = FacadeApp.getAppInstance();
@@ -20,12 +28,12 @@ public class InterfazExamenController {
     // It doesn't matter where you place this code.
     // Controller, App or wherever you'll use it, always will be the same
     // with the same list of reviews
+    @FXML
     public ListView ListaComentarios;
     public List<Review> comentarios=new ArrayList<>();
-    @FXML
-    private Label welcomeText;
 
-    @FXML
+
+   /* @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
         // An example of how to use the FacadeApp
@@ -36,9 +44,11 @@ public class InterfazExamenController {
         System.out.println("--- Review List from Controller ---");
         System.out.println(fApp.getReviewList());
         System.out.println("---------------------------------");
-    }
+    }*/
 
     public void onCrearButtonClick(ActionEvent actionEvent) {
+        Review reviewCreada=new Review(NombreAutor.getText(),contenido.getText());
+        fApp.addReview(reviewCreada);
         
     }
 
